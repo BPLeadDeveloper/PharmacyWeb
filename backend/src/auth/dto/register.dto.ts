6d9cb27e4e98b9695356adfbb13d04b219094a03
Aswathaman K <kashwathaman@gmail.com>
2026-01-22 13:10:56 +0000

    minor changes to auth dtos 2

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,20 +1,25 @@
-import { IsEmail, IsString, MinLength, IsDateString, IsOptional } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsDateString, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   phone: string;
 
   @IsString()
+  @IsNotEmpty()
   @MinLength(8)
   password: string;
 
+  @IsNotEmpty()
   @IsString()
   first_name: string;
 
   @IsString()
+  @IsNotEmpty()
   last_name: string;
 
   @IsDateString()
